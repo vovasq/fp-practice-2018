f989a00b489fc08ba4fc040034e4d10d49fa05f9
vovasq <vova.balsutev@mail.ru>
2018-10-10 11:23:32 +0300

    first lab is done

--- Task1_1.hs
+++ Task1_1.hs
@@ -2,26 +2,57 @@ module Task1_1 where
 
 import Todo(todo)
 
+data Operation = Plus | Minus | Mult deriving (Show, Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ operation :: Operation, lhv :: Term, rhv :: Term } -- бинарная операция
             deriving(Show,Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) l r = BinaryTerm Plus l r
+
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) l r = BinaryTerm Minus l r
+
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) l r = BinaryTerm Mult l r
+
+infixl 6 |+|
+infixl 6 |-|
+infixl 7 |*|
+
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression = 
+  let replace hv = replaceVar varName replacement hv in
+      case expression of
+        Variable variable | variable == varName -> replacement
+        BinaryTerm operartion lhv rhv -> BinaryTerm operartion (replace lhv) (replace rhv)
+        _ -> expression 
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate expression = case expression of
+  BinaryTerm operartion lhs rhs ->
+      case (operartion, left, right) of 
+        (Plus, IntConstant left, IntConstant right) -> IntConstant (left + right)
+        (Minus, IntConstant left, IntConstant right) -> IntConstant (left - right)
+        (Mult, IntConstant left, IntConstant right) -> IntConstant (left * right)
+        (Plus, IntConstant 0, right) -> right
+        (Mult, IntConstant 1, right) -> right
+        (Mult, IntConstant 0, right) -> IntConstant 0
+        (Plus, left, IntConstant 0) -> left
+        (Minus, left, IntConstant 0) -> left
+        (Mult, left, IntConstant 0) -> IntConstant 0
+        (Mult, left, IntConstant 1) -> left
+        _ -> BinaryTerm operartion left right 
+        where
+          left  = evaluate lhs
+          right = evaluate rhs
+  _ -> expression
--- Task1_2.hs
+++ Task1_2.hs
@@ -1,22 +1,63 @@
 module Task1_2 where
 
 import Todo(todo)
+import Data.Fixed
+import Prelude hiding (sin, cos, gcd)
 
 -- синус числа (формула Тейлора)
+-- sin x = (-1) ^ n * x ^ (2 * n + 1) / (2 * n + 1)!  
+-- sin x = x - x ** 3 / (2 * 3) + x ** 5 / (5!)
+
+denomSin :: Double -> Double
+denomSin n = (2 * n * (2 * n + 1)) 
+
+sinus :: Double -> Double -> Double -> Double
+sinus x x_i n = 
+  let precision = 0.0000001 
+      x_i_1 = x_i * x * x * (-1) / (denomSin n) in
+      if (abs x_i) > precision then 
+        x_i + (sinus x x_i_1 (n + 1))
+      else 0.0
+
 sin :: Double -> Double
-sin x = todo
+sin x =
+  let n = 1 in
+    sinus x x n 
+
+denomCos :: Double -> Double
+denomCos n = (2 * n - 1) * 2 * n 
+
+cosinus :: Double -> Double -> Double -> Double
+cosinus x x_i n = 
+  let precision = 0.0000001 
+      x_i_1 = x_i * x * x * (-1) / (denomCos n) in
+      if (abs x_i) > precision then 
+        x_i + (cosinus x x_i_1 (n + 1))
+      else 0.0
 
 -- косинус числа (формула Тейлора)
 cos :: Double -> Double
-cos x = todo
+cos x = 
+  let n = 1 in 
+    cosinus x 1 n
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y = 
+  if x == 0 then y else
+    if y == 0 then x else
+      if x > y then gcd (mod x y) y
+      else gcd x (mod y x)
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
-doesSquareBetweenExist from to = todo
+doesSquareBetweenExist from to 
+  | from == to    = False
+  | mod' x 1 == 0 = True 
+  | otherwise = doesSquareBetweenExist (from + 1) to
+  where x = sqrt (fromIntegral from)
+
+
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
@@ -28,9 +69,23 @@ isDateCorrect day month year = todo
 pow :: Integer -> Integer -> Integer
 pow x y = todo
 
+isPrimeRecurs :: Integer -> Integer -> Bool
+isPrimeRecurs n i = 
+  if i * i < n then
+    if (mod n i) == 0 then False
+    else isPrimeRecurs n (i + 2)
+  else True
+
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+isPrime 2 = True
+isPrime 3 = True
+isPrime n = 
+  if (mod n 6) == 1 || (mod n 6) == 5 then
+    isPrimeRecurs n 5
+  else
+    False
+
 
 type Point2D = (Double, Double)
 
@@ -45,5 +100,9 @@ shapeArea points = todo
 --  1, если он остроугольный
 --  2, если он прямоугольный
 --  -1, если это не треугольник
+-- getCosFromScalarProd :: Point2D -> Point2D -> Double
+-- getCosFromScalarProd a b =
+--   let  
+
 triangleKind :: Point2D -> Point2D -> Point2D -> Integer
 triangleKind a b c = todo
