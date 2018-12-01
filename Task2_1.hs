fbc62ba01198ac157c0d78f3fa5d1c6029b28fc5
vovasqu <vovasq>
2018-12-02 00:34:25 +0300

    task 2 is done

--- Task2_1.hs
+++ Task2_1.hs
@@ -1,49 +1,91 @@
 module Task2_1 where
-
+ 
 import Todo(todo)
+ 
+import Prelude hiding (lookup)
+ 
+
+tree = treeFromList [(7, 93), (1,99), (2,98), (3, 97), (8, 92), (5,95), (6, 94), (4,96)]
 
 -- Ассоциативный массив на основе бинарного дерева поиска
 -- Ключи - Integer, значения - произвольного типа
-
-
-
-data TreeMap v = 
-  Key{ intValue :: Int }
-  | Value{ varName :: String }
-  | BinaryTerm{ left :: TreeMap, right :: TreeMap }
-
+ 
+data TreeMap v = Empty | Node Integer v (TreeMap v) (TreeMap v)
+  deriving (Show, Eq)
+ 
 -- Пустое дерево
-emptyTree :: TreeMap v
-emptyTree = todo
+empty :: TreeMap v
+empty = Empty
 
 -- Содержится ли заданный ключ в дереве?
 contains :: TreeMap v -> Integer -> Bool
-contains t k = todo
+contains Empty _ = False
+contains (Node key _ left right) k
+  | k == key = True
+  | k < key  = contains left k
+  | k > key  = contains right k
 
 -- Значение для заданного ключа
 lookup :: Integer -> TreeMap v -> v
-lookup k t = todo
-
+lookup _ Empty = error "No such an element or Tree is Empty"
+lookup k (Node key value left right)
+  | k < key  = lookup k left
+  | k > key  = lookup k right
+  | k == key = value  
+ 
 -- Вставка пары (ключ, значение) в дерево
 insert :: (Integer, v) -> TreeMap v -> TreeMap v
-insert (k, v) t = todo
-
+insert (newKey, newValue) Empty = Node newKey newValue Empty Empty
+insert (newKey, newValue) (Node key value left right)
+  | newKey < key  = Node key value (insert (newKey, newValue) left) right
+  | newKey > key  = Node key value left (insert (newKey, newValue) right)
+  | newKey == key = error "Already contains this element"
+ 
 -- Удаление элемента по ключу
 remove :: Integer -> TreeMap v -> TreeMap v
-remove i t = todo
-
+remove _ Empty = Empty
+remove i (Node key value left right)
+  | i < key = Node key value (remove i left) right
+  | i > key = Node key value left (remove i right)
+  | otherwise = case (left, right) of
+    (Empty, Empty) -> Empty
+    (left, Empty)  -> left
+    (Empty, right) -> right
+    (left, right)  -> mergeTrees left right
+    where 
+      mergeTrees l Empty = l
+      mergeTrees l (Node key value Empty right) = Node key value l right
+      mergeTrees l (Node key value left right) = Node key value (mergeTrees l left) right
+ 
 -- Поиск ближайшего снизу ключа относительно заданного
 nearestLE :: Integer -> TreeMap v -> (Integer, v)
-nearestLE i t = todo
+nearestLE _ Empty = error "No such an element"
+nearestLE i (Node key value left right)
+  | key > i = nearestLE i left
+  | key < i  = case right of
+    (Node key value _ _) | (i == key) -> (key, value)
+    (Node key value _ _) | (i /= key) -> nearestLE i right
+    otherwise -> (key, value)
+  | otherwise = (key, value)
 
 -- Построение дерева из списка пар
 treeFromList :: [(Integer, v)] -> TreeMap v
-treeFromList lst = todo
-
+treeFromList lst = foldr insert Empty lst
+ 
 -- Построение списка пар из дерева
 listFromTree :: TreeMap v -> [(Integer, v)]
-listFromTree t = todo
+listFromTree Empty                       =  []
+listFromTree (Node key value left right) = listFromTree(left) ++ [(key, value)] ++ listFromTree(right)
 
--- Поиск k-той порядковой статистики дерева 
+size :: TreeMap v -> Integer
+size Empty = 0
+size (Node _ _ left right) = 1 + (size left)  + (size right)
+
+-- Поиск k-той порядковой статистики дерева
 kMean :: Integer -> TreeMap v -> (Integer, v)
-kMean i t = todo
+kMean _ Empty = error "Tree is empty"
+kMean i (Node key value left right)
+  | (size left) == i = (key, value)
+  | (size left) > i  = kMean i left
+  | otherwise     = kMean (i - (size left) - 1) right
+ 
--- Task2_2.hs
+++ Task2_2.hs
@@ -1,60 +1,84 @@
 module Task2_2 where
-
+ 
 import Todo(todo)
-
-import Prelude hiding (foldl, foldr, unfoldr, map, concatMap, 
+import Data.Maybe
+ 
+import Prelude hiding (foldl, foldr,  map, concatMap, unfoldr,
     filter, maxBy, minBy, reverse, sum, product, elem)
-
+ 
 foldl :: (b -> a -> b) -> b -> [a] -> b
-foldl = todo
-
+foldl foo ini []     = ini  
+foldl foo ini (l:ls) = foldl foo (foo ini l) ls
+ 
+ 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr = todo
-
+foldr foo ini []     = ini
+foldr foo ini (l:ls) = foo l (foldr foo ini ls)
+-- (1 'foo' (2 'foo' (3 'foo' ini)))
+ 
 unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-unfoldr = todo
-
+unfoldr foo ini = unfoldr' (foo ini) where
+  unfoldr' (Just (x, ini')) = x : unfoldr foo ini'
+  unfoldr' Nothing          = []
+ 
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
 sum lst = foldl (+) 0 lst
-
+ 
 -- Переворот списка (Пример)
 reverse :: [a] -> [a]
-reverse lst = foldl f [] lst where f t h = h:t
-
+reverse ls = foldl f [] ls 
+  where f t h = h:t
+ 
 -- Отображение элементов списка
 map :: (a -> b) -> [a] -> [b]
-map = todo
-
+map foo lst =
+ foldr (mapper foo) [] lst where mapper foo x y = (foo x):y  
+ 
 -- Произведение всех элементов списка
 product :: [Integer] -> Integer
-product = todo
-
+product []  = 0
+product ls  = foldr (*) 1 ls
+ 
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
-catMaybes = todo
-
+catMaybes = foldr foo [] where
+ foo l ls = case l of Nothing -> ls
+                      Just l  -> l:ls
+                     
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal = todo
-
+diagonal lst = snd(foldr foo ((length lst) - 1, []) lst) where
+ foo x (i, s) = (i - 1, x !! i : s)
+ 
+ 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
-filterNot = todo
-
+filterNot predicate ls = foldr (checkIfPred predicate) [] ls where
+ checkIfPred predicate l ls = if predicate l then l:ls else ls        
+ 
 -- Поиск элемента в списке
 elem :: (Eq a) => a -> [a] -> Bool
-elem = todo
-
+elem el ls = foldr (ifIn el) False ls where
+ ifIn el l ls = if el == l then True else ls
+ 
 -- Список чисел в диапазоне [from, to) с шагом step
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
-rangeTo from to step = todo
-
+rangeTo from to step = unfoldr rangeToFoo from where
+ rangeToFoo from = if from < to then Just (from, from + step)
+                    else Nothing
+  
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
-append = todo
-
+append as [] = as
+append as bs = let concater as bs = as : bs in    
+ foldr concater as bs
+ 
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
+
 groups :: [a] -> Integer -> [[a]]
-groups lst n = todo
+groups lst n = unfoldr (foo n) lst where
+  foo n x  = 
+    if length x == 0 then Nothing
+      else Just(take (fromIntegral n) x , drop (fromIntegral n) x)
