0bc64d2713fba6e97f34d289f6072caf0a21947a
vovasq <vova.balsutev@mail.ru>
2019-01-17 20:16:53 +0300

    task 5_1 always pay your debts

--- Task5_1.hs
+++ Task5_1.hs
@@ -4,13 +4,13 @@ import Todo(todo)
 
 data DList a = DNil 
              | DCons { 
-                left :: (DList a), 
+                left    :: (DList a), 
                 current :: a, 
-                right :: (DList a) 
+                right   :: (DList a) 
              }
 
 instance (Show a) => Show (DList a) where
-    show it = "[" ++ showBody it ++ "]"
+    show it = "Dlist [" ++ showBody it ++ "]"
               where showBody DNil = ""
                     showBody (DCons _ h DNil) = show h
                     showBody (DCons _ h t) = show h ++ ", " ++ showBody t
@@ -21,19 +21,46 @@ instance (Eq a) => Eq (DList a) where
     _ == _ = False
 
 list2dlist :: [a] -> DList a
-list2dlist lst = list2dlist' DNil lst
-
-list2dlist' :: DList a -> [a] -> DList a
-list2dlist' _ [] = DNil
-list2dlist' left (h: t) = 
-    let rec = DCons left h (list2dlist' rec t)
-    in rec
+list2dlist lst = list2dlist' DNil lst 
+  where 
+    -- list2dlist' :: DList a -> [a] -> DList a
+        list2dlist' _ [] = DNil
+        list2dlist' left (h: t) = 
+         let rec = DCons left h (list2dlist' rec t)
+         in rec 
 
 index :: DList a -> Int -> a
-index = todo
+index dlst i = if i < 0 then error "index less zero" 
+  else getIndex i dlst 0 where 
+    getIndex i DNil cnt          = if i >= cnt then error "index out of bound" 
+      else error "kek" 
+    getIndex i (DCons _ h t) cnt =  
+      if cnt == i then h else getIndex i t (cnt + 1)       
+
+sizeD :: DList a -> Int 
+sizeD dlst = sizeCounter 0 dlst
+  where sizeCounter cnt DNil = cnt
+        sizeCounter cnt (DCons _ h t) = sizeCounter (cnt + 1) t 
+
+checkIfWrongIndex :: DList a -> Int -> Bool
+checkIfWrongIndex dlst index | (sizeD dlst <= index || index < 0) =  True
+                             | otherwise = False
+   
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt dlst index value = 
+  if checkIfWrongIndex dlst index then error "wrong index"
+  else inserter dlst index value where
+    inserter DNil 0 v          = DCons DNil v DNil
+    inserter (DCons l h r) 0 v = DCons l v (DCons DNil h r) 
+    inserter (DCons l h r) i v = DCons l h (inserter r (i - 1) v)  
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt dlst index = 
+  if checkIfWrongIndex dlst index then error "wrong index"
+  else remover dlst index where 
+    -- remover (DCons (DCons _ _ _ ) _ _ ) _ = error "pattern error for real" 
+    remover (DCons _ v DNil) 0                 = DNil   
+    remover (DCons l v (DCons _ rv r)) 0    = DCons l rv r 
+    -- remover (DCons DNil v (DCons DNil rv DNil)) 0 = DCons DNil rv DNil 
+    remover (DCons l v r) cnt                  = DCons l v (remover r (cnt - 1)) 
