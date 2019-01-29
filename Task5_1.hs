21e9d6ee8f85109a8ce8e9cb83d49f0ae54fe2b0
vovasq <vova.balsutev@mail.ru>
2019-01-29 23:59:38 +0300

    Revert "update dlist och slozhna" to previous version
    This reverts commit 1fccca208c09b511f105e118cb6f1b3b3345add4.

--- Task5_1.hs
+++ Task5_1.hs
@@ -20,7 +20,6 @@ instance (Eq a) => Eq (DList a) where
     (DCons _ h1 t1) == (DCons _ h2 t2) = h1 == h2 && t1 == t2
     _ == _ = False
 
-
 list2dlist :: [a] -> DList a
 list2dlist lst = list2dlist' DNil lst 
   where 
@@ -46,32 +45,15 @@ sizeD dlst = sizeCounter 0 dlst
 checkIfWrongIndex :: DList a -> Int -> Bool
 checkIfWrongIndex dlst index | (sizeD dlst <= index || index < 0) =  True
                              | otherwise = False
-
-updateLeft DNil _ = DNil
-updateLeft (DCons l c r) left = 
-    let new = DCons left c (updateLeft r new)
-    in new
-
+   
 
 insertAt :: DList a -> Int -> a -> DList a
 insertAt dlst index value = 
   if checkIfWrongIndex dlst index then error "wrong index"
-  else inserter  dlst index value where
+  else inserter dlst index value where
     inserter DNil 0 v          = DCons DNil v DNil
-    -- inserter (DCons l h r) 0 v = updateLeft (DCons l v (DCons DNil h r)) (DCons DNil h r)  
     inserter (DCons l h r) 0 v = DCons l v (DCons DNil h r) 
-    inserter (DCons l h r) i v = updateLeft (DCons l h (inserter r (i - 1) v)) (inserter r (i - 1) v)   
-    -- inserter (DCons l h r) i v = DCons l h (inserter r (i - 1) v)
-
-
-
--- [1, 2]
--- dnil 1 (dcons (dnil 1...) )
-  
--- [1,2] -> DCons DNil 1 ((DNil) 2 DNil)
--- [1,2,3]
-
--- 1 2 3 4 5 6 7 8 9 10
+    inserter (DCons l h r) i v = DCons l h (inserter r (i - 1) v)  
 
 removeAt :: DList a -> Int -> DList a
 removeAt dlst index = 
@@ -81,9 +63,4 @@ removeAt dlst index =
     remover (DCons _ v DNil) 0                 = DNil   
     remover (DCons l v (DCons _ rv r)) 0    = DCons l rv r 
     -- remover (DCons DNil v (DCons DNil rv DNil)) 0 = DCons DNil rv DNil 
-    remover (DCons l v r) cnt                  = updateLeft (DCons l v (remover r (cnt - 1))) (DCons l v (remover r (cnt - 1)))   
-
-
-dls   = list2dlist [1..5]
-dls6  = insertAt dls 1 777
-check = left (right dls6)
+    remover (DCons l v r) cnt                  = DCons l v (remover r (cnt - 1)) 
